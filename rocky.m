e5779b0c4eb73f2b4c3b5865b36f0c3f19c660c8
julakshah <julakshah@gmail.com>
2026-03-08 20:08:13 -0400

    rocky script with code for fitting the motor data for alpha and c--FIXED

--- rocky.m
+++ rocky.m
@@ -1,10 +1,13 @@
 function main
-
+    [t, y_L, v_L, y_R, v_R] = load_motor_data()
+    range = [100:200]
+    single_v_L = v_L(range)
+    single_t = t(range) - t(range(1))
+    fitData(single_t, single_v_L);
 end
 
 
-function fitData
-    % Fit: '3b'.
+function fitData(t, v_L)
     [xData, yData] = prepareCurveData( t, v_L );
     
     % Set up fittype and options.
@@ -14,7 +17,7 @@ function fitData
     opts.StartPoint = [1 0.0461713906311539];
     
     % Fit model to data.
-    [fitresult, gof] = fit( xData, yData, ft, opts );
+    [fitresult, gof] = fit( xData, yData, ft, opts )
     
     % Plot fit with data.
     figure( 'Name', 'untitled fit 1' );
@@ -24,4 +27,34 @@ function fitData
     xlabel( 't', 'Interpreter', 'none' );
     ylabel( 'v_L', 'Interpreter', 'none' );
     grid on
+end
+
+function [t, y_L, v_L, y_R, v_R] = load_motor_data()
+    %path and file name of data
+    fpath = './'; %path (change this!)
+    fname_in = 'motor_calib.txt'; %file name (change this!)
+    %load the motor calibration data
+    motor_data = importdata([fpath,fname_in]);
+    %unpack the motor calibration data
+    t = motor_data(:,1);
+    y_L = motor_data(:,2); v_L = motor_data(:,3);
+    y_R = motor_data(:,4); v_R = motor_data(:,5);
+    %plot the motor calibration data
+    figure(1);
+    subplot(2,1,1);
+    hold on
+    plot(t,v_L,'c','linewidth',1);
+    plot(t,v_R,'r','linewidth',1);
+    xlabel('time (sec)'); ylabel('wheel speed (m/sec)');
+    title('Motor Calibration Data');
+    h1 = legend('Left Wheel','Right Wheel');
+    set(h1,'location','southeast');
+    subplot(2,1,2);
+    hold on
+    plot(t,y_L,'c','linewidth',1);
+    plot(t,y_R,'r--','linewidth',1);
+    xlabel('time (sec)'); ylabel('wheel command (-)');
+    title('Motor Calibration Data');
+    h2 = legend('Left Wheel','Right Wheel');
+    set(h2,'location','southeast');
 end
